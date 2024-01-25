51d81c18d46d7ff5843de95a65330fd8e3ac1091
missorforget <lymissorforget@gmail.com>
2024-01-25 08:51:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,79 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+mkdir Modem-Support
+pushd Modem-Support
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support .
+popd
+
+mkdir MyConfig
+pushd MyConfig
+git clone --depth=1 https://github.com/Siriling/OpenWRT-MyConfig .
+popd
+
+mkdir package/community
+pushd package/community
+
+# 5G通信模组拨号工具
+mkdir quectel_QMI_WWAN
+mkdir quectel_cm_5G
+mkdir quectel_MHI
+# mkdir luci-app-hypermodem
+cp -rf ../../Modem-Support/quectel_QMI_WWAN/* quectel_QMI_WWAN
+cp -rf ../../Modem-Support/quectel_cm_5G/* quectel_cm_5G
+cp -rf ../../Modem-Support/quectel_MHI/* quectel_MHI
+# cp -rf ../../Modem-Support/luci-app-hypermodem/* luci-app-hypermodem
+
+# 5G模组短信插件
+mkdir sms-tool
+mkdir luci-app-sms-tool
+cp -rf ../../Modem-Support/sms-tool/* sms-tool
+cp -rf ../../Modem-Support/luci-app-sms-tool/* luci-app-sms-tool
+cp -rf ../../MyConfig/configs/istoreos/general/applications/luci-app-sms-tool/* luci-app-sms-tool
+
+# 5G模组信息插件+AT工具
+mkdir luci-app-modem
+cp -rf ../../Modem-Support/luci-app-modem/* luci-app-modem
+
+popd
+
+#5G相关
+echo "
+# 5G模组信号插件
+# CONFIG_PACKAGE_ext-rooter-basic=y
+
+# 5G模组短信插件
+CONFIG_PACKAGE_luci-app-sms-tool=y
+
+# 5G模组信息插件
+# CONFIG_PACKAGE_luci-app-3ginfo-lite=y
+# CONFIG_PACKAGE_luci-app-3ginfo=y
+
+# 5G模组信息插件+AT工具
+# CONFIG_PACKAGE_luci-app-cpe=y
+# CONFIG_PACKAGE_sendat=y
+CONFIG_PACKAGE_luci-app-modem=y
+
+# QMI拨号工具（移远，广和通）
+# CONFIG_PACKAGE_quectel-CM-5G=y
+# CONFIG_PACKAGE_fibocom-dial=y
+
+# QMI拨号软件
+# CONFIG_PACKAGE_kmod-qmi_wwan_f=y
+# CONFIG_PACKAGE_luci-app-hypermodem=y
+
+# Gobinet拨号软件
+# CONFIG_PACKAGE_kmod-gobinet=y
+# CONFIG_PACKAGE_luci-app-gobinetmodem=y
+
+# 串口调试工具
+CONFIG_PACKAGE_minicom=y
+
+# 脚本拨号工具依赖
+CONFIG_PACKAGE_grep=y
+CONFIG_PACKAGE_procps-ng=y
+CONFIG_PACKAGE_procps-ng-ps=y
+
+" >> .config
